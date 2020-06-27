db6527cdbf606ee4b36377e7385b0e6a198bf3a1
MrFaiman <57595937+MrFaiman@users.noreply.github.com>
2020-06-27 14:05:52 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+client_script({'client.lua'})
+
+
+ui_page('html/index.html')
+
+
+files({
+    'html/index.html',
+    'html/img/crouch.png',
+    'html/img/stand.png',
+    'html/img/prone.png'
+})
